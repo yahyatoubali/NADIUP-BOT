f20921c60351b36957d0b11951d0572badadc1e0
YAHYA TOUBALI <yahyaboboyahya@gmail.com>
2024-07-04 17:30:20 +0100

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -78,7 +78,6 @@ async def handle_user_input(bot: Client, update: Message):
     else:
         await update.reply_text("I don't understand this input type. Please provide a URL, a magnet link, or a file.")
 
-
 async def process_direct_link(bot: Client, update: Message):
     """Handles direct URLs (regular HTTP/HTTPS links)."""
     url = update.text
@@ -155,92 +154,90 @@ async def process_direct_link(bot: Client, update: Message):
             reply_to_message_id=update.id,
             parse_mode=enums.ParseMode.HTML
           )
-    process = await asyncio.create_subprocess_exec(
-        *command_to_exec,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    logger.info(e_response)
-    t_response = stdout.decode().strip()
-    if e_response and "nonnumeric port" not in e_response:
-        # logger.warn("Status : FAIL", exc.returncode, exc.output)
-        error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
-        if "This video is only available for registered users." in error_message:
-            error_message += Translation.SET_CUSTOM_USERNAME_PASSWORD
-        await chk.delete()
-        time.sleep(1)
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_message_id=update.id,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True
+    try: # The try block starts here
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
-        return False
-    if t_response:
-        # logger.info(t_response)
-        x_reponse = t_response
-        if "\n" in x_reponse:
-            x_reponse, _ = x_reponse.split("\n")
-        response_json = json.loads(x_reponse)
-        randem = random_char(5)
-        save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
-            "/" + str(update.from_user.id) + f'{randem}' + ".json"
-        with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
-            json.dump(response_json, outfile, ensure_ascii=False)
-        # logger.info(response_json)
-        inline_keyboard = []
-        duration = None
-        if "duration" in response_json:
-            duration = response_json["duration"]
-        if "formats" in response_json:
-            for formats in response_json["formats"]:
-                format_id = formats.get("format_id")
-                format_string = formats.get("format_note")
-                if format_string is None:
-                    format_string = formats.get("format")
-                format_ext = formats.get("ext")
-                approx_file_size = ""
-                if "filesize" in formats:
-                    approx_file_size = humanbytes(formats["filesize"])
-                cb_string_video = "{}|{}|{}|{}".format(
-                    "video", format_id, format_ext, randem)
-                cb_string_file = "{}|{}|{}|{}".format(
-                    "file", format_id, format_ext, randem)
-                if format_string is not None and not "audio only" in format_string:
-                    ikeyboard = [
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+
+        e_response = stderr.decode().strip()
+        logger.info(e_response)
+        t_response = stdout.decode().strip()
+        if e_response and "nonnumeric port" not in e_response:
+            error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
+            if "This video is only available for registered users." in error_message:
+                error_message += Translation.SET_CUSTOM_USERNAME_PASSWORD
+            await chk.delete()
+            time.sleep(1)
+            await bot.send_message(
+                chat_id=update.chat.id,
+                text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
+                reply_to_message_id=update.id,
+                parse_mode=enums.ParseMode.HTML,
+                disable_web_page_preview=True
+            )
+            return False
+        if t_response:
+            x_reponse = t_response
+            if "\n" in x_reponse:
+                x_reponse, _ = x_reponse.split("\n")
+            response_json = json.loads(x_reponse)
+            randem = random_char(5)
+            save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
+                "/" + str(update.from_user.id) + f'{randem}' + ".json"
+            with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
+                json.dump(response_json, outfile, ensure_ascii=False)
+            inline_keyboard = []
+            duration = None
+            if "duration" in response_json:
+                duration = response_json["duration"]
+            if "formats" in response_json:
+                for formats in response_json["formats"]:
+                    format_id = formats.get("format_id")
+                    format_string = formats.get("format_note")
+                    if format_string is None:
+                        format_string = formats.get("format")
+                    format_ext = formats.get("ext")
+                    approx_file_size = ""
+                    if "filesize" in formats:
+                        approx_file_size = humanbytes(formats["filesize"])
+                    cb_string_video = "{}|{}|{}|{}".format(
+                        "video", format_id, format_ext, randem)
+                    cb_string_file = "{}|{}|{}|{}".format(
+                        "file", format_id, format_ext, randem)
+                    if format_string is not None and not "audio only" in format_string:
+                        ikeyboard = [
+                            InlineKeyboardButton(
+                                f"🎬 {format_string} {format_ext} {approx_file_size} ",
+                                callback_data=(cb_string_video).encode("UTF-8")
+                            )
+                        ]
+                    else:
+                        # Special weird case :
+                        ikeyboard = [
+                            InlineKeyboardButton(
+                                f"🎬 [{format_ext}] ({approx_file_size})",
+                                callback_data=(cb_string_video).encode("UTF-8")
+                            )
+                        ]
+                    inline_keyboard.append(ikeyboard)
+                if duration is not None:
+                    cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", randem)
+                    cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", randem)
+                    cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", randem)
+                    inline_keyboard.append([
                         InlineKeyboardButton(
-                            f"🎬 {format_string} {format_ext} {approx_file_size} ",
-                            callback_data=(cb_string_video).encode("UTF-8")
-                        )
-                    ]
-                else:
-                    # Special weird case :
-                    ikeyboard = [
+                            f"🎵 MP3 (64 kbps)", callback_data=cb_string_64.encode("UTF-8")),
                         InlineKeyboardButton(
-                            f"🎬 [{format_ext}] ({approx_file_size})",
-                            callback_data=(cb_string_video).encode("UTF-8")
-                        )
-                    ]
-                inline_keyboard.append(ikeyboard)
-            if duration is not None:
-                cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", randem)
-                cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", randem)
-                cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", randem)
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        f"🎵 MP3 (64 kbps)", callback_data=cb_string_64.encode("UTF-8")),
-                    InlineKeyboardButton(
-                        f"🎵 MP3 (128 kbps)", callback_data=cb_string_128.encode("UTF-8"))
-                ])
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        f"🎵 MP3 (320 kbps)", callback_data=cb_string.encode("UTF-8"))
-                ])
+                            f"🎵 MP3 (128 kbps)", callback_data=cb_string_128.encode("UTF-8"))
+                    ])
+                    inline_keyboard.append([
+                        InlineKeyboardButton(
+                            f"🎵 MP3 (320 kbps)", callback_data=cb_string.encode("UTF-8"))
+                    ])
                 inline_keyboard.append([                 
                     InlineKeyboardButton(
                         "⛔️ Close", callback_data='close')               
@@ -295,7 +292,7 @@ async def process_direct_link(bot: Client, update: Message):
                 reply_to_message_id=update.id
             )
 
-    except Exception as e:
+    except Exception as e: # Correct indentation
         logger.error(f"An error occurred: {e}")
         await chk.delete()
         await bot.send_message(
