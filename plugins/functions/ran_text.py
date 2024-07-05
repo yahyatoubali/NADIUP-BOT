682d7ff25346cffb72a9bcf4b1da4001a15c54cf
YAHYA TOUBALI <yahyaboboyahya@gmail.com>
2024-07-05 20:54:59 +0100

    Update ran_text.py

--- plugins/functions/ran_text.py
+++ plugins/functions/ran_text.py
@@ -1,10 +1,12 @@
 # ᴛʜᴀɴᴋs
-
 import random
 import string
 
-def random_char(y):
-       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+def random_char(length: int = 5) -> str:
+    """Generates a random string of the specified length."""
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+# ... (You can keep your `ran` variable if you are using it) 
 
 ran = (random_char(5)) 
 
